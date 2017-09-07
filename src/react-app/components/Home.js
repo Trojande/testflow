9eba764c8a08b3780926880d026210cde004fd2e
Artur Maul <trojan.de09@gmail.com>
2017-09-07 21:28:45 +0600

    delete unused import

--- src/react-app/components/Home.js
+++ src/react-app/components/Home.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react';
-import { Router } from 'react-router';
-import {
-    Link,
-} from 'react-router-dom';
-import Authorization from './Authorization/index';
-import reducers from '../reducers';
 
 class Home extends Component {
 
